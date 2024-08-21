3922310d89032564d6b3e3a10691f93bfd49fcf2
kokkaming0115 <kkmm0115@outlook.com>
2024-08-21 02:48:43 +0000

    covert JSON to Object

--- src/main/java/com/example/demo/AccountWallet.java
+++ src/main/java/com/example/demo/AccountWallet.java
@@ -1,12 +1,20 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.util.function.Consumer;
 
 @SpringBootApplication
 public class AccountWallet {
+
+	@Autowired
+	private ObjectMapper om;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountWallet.class, args);
@@ -22,11 +30,19 @@ public class AccountWallet {
     public Consumer<String> sink(){
         return txtMsg -> {
 			System.out.println(txtMsg);
+			AccountTxn accountTxn=null;
 			//JSON to Object
-
+			try {
+				accountTxn = om.readValue(txtMsg, AccountTxn.class);
+			} catch (JsonProcessingException e) {
+				
+				e.printStackTrace();
+			}
+			System.out.println("accountTxn=>"+accountTxn);
 			//Save to Ledge
-
+			
 			//Update Cache
+			throw new RuntimeException("test error!");
 		};
     }
 
